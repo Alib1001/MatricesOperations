7e53d1e2d7199426dc10992117036cdada67d6f4
Alib281 <superalibek123@gmail.com>
2023-02-13 17:42:34 +0600

    transposeMatrix function added

--- src/main/java/org/alib/matrices/Matrix.java
+++ src/main/java/org/alib/matrices/Matrix.java
@@ -36,14 +36,27 @@ class Matrix {
 
 
     public static void displayMatrix(int[][] matrix) {
-        //TODO переписать фунцию для того, чтобы при разном кол-ве столбцов отображение было верным
         System.out.println("Матрица:");
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
                 System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
     }
+
+    public int[][] transposeMatrix() {
+        if (matrix.length > 0) {
+            System.out.println("Transposed matrix:");
+        }
+        int[][] transposedMatrix = new int[matrix[0].length][matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                transposedMatrix[j][i] = matrix[i][j];
+            }
+        }
+        return transposedMatrix;
+    }
+
 
 }
